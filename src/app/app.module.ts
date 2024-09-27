1c8e4dff9301c4600803c80afc433d3758331ae8
Valerii Ihnatiev <valerij.ignatjev@gmail.com>
2024-09-28 01:46:12 +0300

    Created more complex Module-based APP structures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { TaskComponent } from "./tasks/components/task/task.component";
-import { NewTaskComponent } from "./tasks/components/new-task/new-task.component";
-import { FormsModule } from "@angular/forms";
 import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent,],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule]
+  imports: [BrowserModule, FormsModule, SharedModule, TasksModule]
 })
 export class AppModule {
 }
